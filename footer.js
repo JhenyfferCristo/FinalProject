5fbf6fe1c4fcd9f37f22239cdff5bb28ba4af418
Jhenyffer Cristo Marques <79174700+JhenyfferCristo@users.noreply.github.com>
2023-04-28 21:27:01 -0600

    Add files via upload

--- footer.js
+++ footer.js
@@ -3,12 +3,12 @@ function repeatFooter() {
     '<div class="imgBox">'+
       '<div class="footerImages">'+
         '<a href="https://www.facebook.com/">'+
-          '<img src="logo-img/facebook.png" alt="Facebook"'+
+          '<img src="./logo-img/facebook.png" alt="Facebook"'+
         '/></a>'+
       '</div>'+
       '<div class="footerImages">'+
        ' <a href="https://www.instagram.com/">'+
-          '<img src="logo-img/instagram.png" alt="Instagram"'+
+          '<img src="./logo-img/instagram.png" alt="Instagram"'+
         '/></a>'+
       '</div>'+
     '</div>';
