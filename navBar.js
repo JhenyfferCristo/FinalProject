06dade6d564986a77408f53f062fc8a47f568c0b
Jhenyffer Cristo Marques <79174700+JhenyfferCristo@users.noreply.github.com>
2023-04-28 21:18:06 -0600

    Add files via upload

--- navBar.js
+++ navBar.js
@@ -1,21 +1,21 @@
 function repeatNavBar() {
     var navBarHTML = 
                         '<div class="openinglogo">' +
-                          '<a href="/index.html">'+
-                          '<img src="/logo-img/logo-flor.PNG" alt="flower" /></a>' +
+                          '<a href="./index.html">'+
+                          '<img src="./logo-img/logo-flor.PNG" alt="flower" /></a>' +
                           '<p>Green Thumb Landscape</p>' +
                         '</div>' +
                         '<nav>' +
                           '<div class="dropdown">' +
                             '<button class="activeMenu">Menu</button>' +
                             '<ul class="dropdown-itens">' +
-                              '<li><a href="/About.html">About Us</a></li>' +
-                              '<li><a href="/services.html">Services</a></li>' +
-                              '<li><a href="/contact.html">Contact</a></li>' +
-                              '<li><a href="/review.html">Testimonials</a></li>' +
+                              '<li><a href="./About.html">About Us</a></li>' +
+                              '<li><a href="./services.html">Services</a></li>' +
+                              '<li><a href="./contact.html">Contact</a></li>' +
+                              '<li><a href="./review.html">Testimonials</a></li>' +
                             '</ul>' +
                           '</div>' +
-                          '<a href="quote.html"><button class="quoteButton">Quote</button></a>' +
+                          '<a href="./quote.html"><button class="quoteButton">Quote</button></a>' +
                         '</nav>';
   
     return navBarHTML;
